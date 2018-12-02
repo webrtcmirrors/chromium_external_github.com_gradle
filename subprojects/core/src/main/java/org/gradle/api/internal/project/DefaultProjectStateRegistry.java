7a6c66889a1d961f90aad480c92af7cbdc65a776
Adam Murdoch <adam@gradle.com>
2018-12-03 07:47:38 +1100

    Don't use a static `ThreadLocal` to track lenient access to the projects of a build, as entitlement to access the projects of one build does not imply any right to access the projects of any other build.

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProjectStateRegistry.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProjectStateRegistry.java
@@ -42,7 +42,7 @@ public class DefaultProjectStateRegistry implements ProjectStateRegistry {
     private final Map<Path, ProjectStateImpl> projectsByPath = Maps.newLinkedHashMap();
     private final Map<ProjectComponentIdentifier, ProjectStateImpl> projectsById = Maps.newLinkedHashMap();
     private final Map<Pair<BuildIdentifier, Path>, ProjectStateImpl> projectsByCompId = Maps.newLinkedHashMap();
-    private final static ThreadLocal<Boolean> LENIENT_MUTATION_STATE = new ThreadLocal<Boolean>() {
+    private final ThreadLocal<Boolean> lenientMutationState = new ThreadLocal<Boolean>() {
         @Override
         protected Boolean initialValue() {
             return Boolean.FALSE;
@@ -126,12 +126,12 @@ public class DefaultProjectStateRegistry implements ProjectStateRegistry {
 
     @Override
     public <T> T withLenientState(Factory<T> factory) {
-        Boolean originalState = LENIENT_MUTATION_STATE.get();
-        LENIENT_MUTATION_STATE.set(true);
+        Boolean originalState = lenientMutationState.get();
+        lenientMutationState.set(true);
         try {
             return factory.create();
         } finally {
-            LENIENT_MUTATION_STATE.set(originalState);
+            lenientMutationState.set(originalState);
         }
     }
 
@@ -188,7 +188,7 @@ public class DefaultProjectStateRegistry implements ProjectStateRegistry {
 
         @Override
         public <T> T withMutableState(final Factory<? extends T> factory) {
-            if (LENIENT_MUTATION_STATE.get()) {
+            if (lenientMutationState.get()) {
                 return factory.create();
             }
 
@@ -228,7 +228,7 @@ public class DefaultProjectStateRegistry implements ProjectStateRegistry {
 
         @Override
         public boolean hasMutableState() {
-            return LENIENT_MUTATION_STATE.get() || workerLeaseService.getCurrentProjectLocks().contains(projectLock);
+            return lenientMutationState.get() || workerLeaseService.getCurrentProjectLocks().contains(projectLock);
         }
     }
 
